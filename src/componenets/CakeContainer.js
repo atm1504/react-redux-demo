21c03739e58d478df4c6665c733a2005d9a8039f
Amartya Mondal <atm1504.in@gmail.com>
2021-08-05 09:36:51 +0530

    Basic setup

---
+++ src/componenets/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CakeContainer() {
+  return (
+    <div>
+      <h2>No of cakes</h2>
+      <h3>No of bites</h3>
+    </div>
+  );
+}
+
+export default CakeContainer;
