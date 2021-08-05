d35e8f22840a517ce8ae3e76cf4972109fc807e9
Amartya Mondal <atm1504.in@gmail.com>
2021-08-05 21:00:36 +0530

    Add hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import CakeContainer from "./componenets/CakeContainer";
-import { Provider } from 'react-redux';
+import HooksCakeContainer from "./componenets/HooksCakeContainer";
+import { Provider } from "react-redux";
 import store from "./redux/store";
 
 function App() {
   return (
-    <Provider store = {store}>
+    <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
